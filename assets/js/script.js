6c32860dd48e1e941fe0cd6788c5e3b67bb14f53
ianaack <ianaack@gmail.com>
2022-03-23 12:14:24 -0400

    Feature: able to pull a response from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,57 @@
-var searchFormEl = document.querySelector("#searchForm");
-var cityInputEl = document.querySelector("#citySearch");
-var currentWeatherEl = document.querySelector("#currentWeather");
+// global variables
+var formEl = document.getElementById("searchForm");
+var cityInputEl = document.getElementById("citySearch");
+var searchBtnEl = document.getElementById("searchBtn");
 var apiKey = "04ca341e77d18f208b89180571b95af1";
-var city = cityInputEl.value.trim();
 
-var formSubmitHandler = function (event) {
-  // prevent page from refreshing
+formEl.addEventListener("submit", (event) => {
+  // handle the form data
   event.preventDefault();
 
   // get value from input element
   var city = cityInputEl.value.trim();
 
   if (city) {
-    getCityWeather(city);
+    getCoordinates(city);
 
-    // clear old content
-    currentWeatherEl.textContnet = "";
+    // clear current weather data and city search box
     cityInputEl.value = "";
   } else {
-    alert("Please enter a City");
+    alert("Please enter a valid city");
   }
-};
+});
 
-var getCityWeather = function (city) {
-  // format the OpenWeather API Url
-  var apiUrl =
-    "https://api.openweathermap.org/geo/1.0/reverse?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&limit=5&appid=" +
-    apiKey;
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayWeather();
-      });
-    }
-  });
-};
-
-var getCoordinateData = function (coordinates) {
-  // format the OpenWeather API Url
-  var apiUrl =
+// convert city value to lat/lon
+var getCoordinates = function () {
+  // format the openweather api url
+  var coordinatesUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
+    cityInputEl.value.trim() +
     "&limit=5&appid=" +
     apiKey;
+
+  // make a get request to url
+  fetch(coordinatesUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          getWeather(data, city, lat, lon);
+        });
+      } else {
+        alert("Error: City Not Found");
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to OpenWeather");
+    });
+
+  // extract lat, lon, and name data to pass into getWeather function
 };
+
+// fetch city info using lat/lon
+
+// display current weather
+// display 5-day forecast
+// localstorage search history
+// create li element for search history
